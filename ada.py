6b7483325dd7ed3b63ca32973f8a54654aca6d33
francoleguizamon2 <ftleguizamon@mail.austral.edu.ar>
2025-03-31 11:49:52 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,13 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    lowercase = first_name.lower() + ' ' + last_name.lower()
+    uppercase = first_name.upper() + ' ' + last_name.upper()
+    title = first_name.title() + ' ' + last_name.title()
+    indentado = '    ' + lowercase
+    print(lowercase)
+    print(title)
+    print(uppercase)
+    print(indentado)
+# Todos estos print producen el output solicitado en el README
+ada()
