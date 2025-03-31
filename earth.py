b9510961e423bfa4ec0ac784bd972dc4139a0942
francoleguizamon2 <ftleguizamon@mail.austral.edu.ar>
2025-03-31 11:50:21 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    diccionario = [x,y] # En realidad es una lista pero conceptualmente es un diccionario
+    diccionario.sort() # Ordena alfabeticamente
+    print(f"The result of Bangladesh comes first in the dictionary than Barbados is {diccionario[0] == x}.")
+    print(f"The result of Barbados comes first in the dictionary than Bangladesh is {diccionario[0] == y}.")
+# El output es el solicitado por el README
+earth()
