100ff685b40d944ee30cc17bbf007bfc90e850e4
francoleguizamon2 <ftleguizamon@mail.austral.edu.ar>
2025-03-31 11:50:08 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
-def change():
-    expense = 23.75
-    money = 100
+def change(expense, money):
+    print('Ingresar gasto: ', expense)
+    print('Dinero recibido: ', money)
+    if expense > money:
+        print('Saldo insuficiente')
+        return False
+    elif expense == money:
+        print('Pagó justo')
+    else:
+        change = money - expense
+        change_entero = int(money - expense)
+        change_centavos = change - (change_entero)
+        print('Vuelto: ', 'Pesos: ', change_entero, 'Centavos: ', change_centavos)
+        return True
+gasto = 23.75
+dinero_recibido = 100
+change(gasto, dinero_recibido)
